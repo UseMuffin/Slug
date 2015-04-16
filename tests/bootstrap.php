329559e5c1801bbd3168e1ab281de9df3c3e59e0
ADmad <admad.coder@gmail.com>
2015-04-16 20:17:39 +0530

    Simplify tests bootstrap

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,89 +1,31 @@
 <?php
-// @codingStandardsIgnoreFile
+/**
+ * Test suite bootstrap.
+ *
+ * This function is used to find the location of CakePHP whether CakePHP
+ * has been installed as a dependency of the plugin, or the plugin is itself
+ * installed as a dependency of an application.
+ */
+$findRoot = function ($root) {
+    do {
+        $lastRoot = $root;
+        $root = dirname($root);
+        if (is_dir($root . '/vendor/cakephp/cakephp')) {
+            return $root;
+        }
+    } while ($root !== $lastRoot);
 
-use Cake\Core\Configure;
-use Cake\Core\Plugin;
-use Cake\Cache\Cache;
-use Cake\Datasource\ConnectionManager;
+    throw new Exception("Cannot find the root of the application, unable to run tests");
+};
+$root = $findRoot(__FILE__);
+unset($findRoot);
 
-require_once 'vendor/autoload.php';
-
-// Path constants to a few helpful things.
-if (!defined('DS')) {
-    define('DS', DIRECTORY_SEPARATOR);
+chdir($root);
+if (file_exists($root . '/config/bootstrap.php')) {
+    require $root . '/config/bootstrap.php';
+    return;
 }
 
-define('ROOT', dirname(__DIR__) . DS);
-define('CAKE_CORE_INCLUDE_PATH', ROOT . 'vendor' . DS . 'cakephp' . DS . 'cakephp');
-define('CORE_PATH', ROOT . 'vendor' . DS . 'cakephp' . DS . 'cakephp' . DS);
-define('CAKE', CORE_PATH . 'src' . DS);
-define('TESTS', ROOT . 'tests');
-define('APP', ROOT . 'tests' . DS . 'test_files' . DS . 'app' . DS);
-define('APP_DIR', 'app');
-define('WEBROOT_DIR', 'webroot');
-define('WWW_ROOT', dirname(APP) . DS . 'webroot' . DS);
-define('TMP', sys_get_temp_dir() . DS);
-define('CONFIG', dirname(APP) . DS . 'config' . DS);
-define('CACHE', TMP);
-define('LOGS', TMP);
+require $root . '/vendor/cakephp/cakephp/tests/bootstrap.php';
 
-$loader = new \Cake\Core\ClassLoader();
-$loader->register();
-
-$loader->addNamespace('Muffin\Slug\TestApp', APP);
-
-require_once CORE_PATH . 'config/bootstrap.php';
-
-date_default_timezone_set('UTC');
-mb_internal_encoding('UTF-8');
-
-Configure::write('debug', true);
-Configure::write('App', [
-    'namespace' => 'Muffin\Slug\TestApp',
-    'encoding' => 'UTF-8',
-    'base' => false,
-    'baseUrl' => false,
-    'dir' => 'src',
-    'webroot' => WEBROOT_DIR,
-    'www_root' => WWW_ROOT,
-    'fullBaseUrl' => 'http://localhost',
-    'imageBaseUrl' => 'img/',
-    'jsBaseUrl' => 'js/',
-    'cssBaseUrl' => 'css/',
-    'paths' => [
-        'plugins' => [dirname(APP) . DS . 'plugins' . DS],
-        'templates' => [APP . 'Template' . DS]
-    ]
-]);
-
-Cache::config([
-    '_cake_core_' => [
-        'engine' => 'File',
-        'prefix' => 'cake_core_',
-        'serialize' => true
-    ],
-    '_cake_model_' => [
-        'engine' => 'File',
-        'prefix' => 'cake_model_',
-        'serialize' => true
-    ],
-    'default' => [
-        'engine' => 'File',
-        'prefix' => 'default_',
-        'serialize' => true
-    ]
-]);
-
-// Ensure default test connection is defined
-if (!getenv('db_dsn')) {
-    putenv('db_dsn=sqlite:///' . TMP . 'muffin_tags_test.sqlite');
-}
-$config = [
-    'url' => getenv('db_dsn'),
-    'timezone' => 'UTC',
-];
-
-// var_dump($config);die();
-ConnectionManager::config('test', $config);
-
-Plugin::load('Muffin/Slug', ['path' => ROOT]);
+\Cake\Core\Plugin::load('Muffin/Slug', ['path' => dirname(dirname(__FILE__)) . DS]);
