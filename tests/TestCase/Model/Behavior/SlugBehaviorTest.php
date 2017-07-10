df4d0e831b4839fe7b4a4ea87c02887de70b8e65
Florian Krämer <florian.kraemer@kreative-design.net>
2017-07-10 16:08:18 +0200

    Adding a test and fixing phpunit

--- tests/TestCase/Model/Behavior/SlugBehaviorTest.php
+++ tests/TestCase/Model/Behavior/SlugBehaviorTest.php
@@ -390,4 +390,22 @@ class SlugBehaviorTest extends TestCase
         $result = $this->Behavior->slug('FOO');
         $this->assertEquals('foo', $result);
     }
+
+    public function testMultipleDisplayFieldsForSlug()
+    {
+        $Articles = TableRegistry::get('Muffin/Slug.Articles', ['table' => 'slug_articles']);
+        TableRegistry::get('Muffin/Slug.Authors', ['table' => 'slug_authors']);
+
+        $Articles->belongsTo('Authors', ['className' => 'Muffin/Slug.Authors']);
+        $Articles->addBehavior('Muffin/Slug.Slug', ['displayField' => ['author.name', 'title']]);
+
+        $entity = $Articles->get(1, [
+            'contain' => [
+                'Authors'
+            ]
+        ]);
+
+        $slug = $Articles->slug($entity);
+        $this->assertEquals('admad-first-article', $slug);
+    }
 }
